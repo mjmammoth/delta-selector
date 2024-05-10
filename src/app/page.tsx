5e028434f84bb950a7d6bd154eca8d9b192360ff
Mark Marryatt <awsumtoast@gmail.com>
2024-05-10 15:03:00 +0200

    Beating the sloppy code allegations
    Signed-off-by: Mark Marryatt <awsumtoast@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
-import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Header } from "../components/header";
 import { Input } from "../components/input";
 import { Select } from "../components/select";
--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,6 +1,4 @@
 import { Checkbox } from "@/components/ui/checkbox"
-// import { SVGProps } from "react"
-// import { Switch } from "@/components/ui/switch"
 
 interface SelectProps {
   items: string[];
@@ -21,12 +19,6 @@ export function Select({ items, selectedItems, setSelectedItems } : SelectProps)
     <div className="space-y-2 m-4">
       <div className="flex items-center justify-between">
         <h3 className="text-lg font-medium">Select</h3>
-        {/*
-        <Switch aria-label="Invert">
-          <FlipVerticalIcon className="mr-2 h-4 w-4" />
-          Invert
-        </Switch>
-        */}
       </div>
       <ul className="space-y-2">
         {items.map(item => (
@@ -41,27 +33,3 @@ export function Select({ items, selectedItems, setSelectedItems } : SelectProps)
     </div>
   );
 }
-
-// function FlipVerticalIcon(props: SVGProps<SVGSVGElement>) {
-//   return (
-//     <svg
-//       {...props}
-//       xmlns="http://www.w3.org/2000/svg"
-//       width="24"
-//       height="24"
-//       viewBox="0 0 24 24"
-//       fill="none"
-//       stroke="currentColor"
-//       strokeWidth="2"
-//       strokeLinecap="round"
-//       strokeLinejoin="round"
-//     >
-//       <path d="M21 8V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v3" />
-//       <path d="M21 16v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-3" />
-//       <path d="M4 12H2" />
-//       <path d="M10 12H8" />
-//       <path d="M16 12h-2" />
-//       <path d="M22 12h-2" />
-//     </svg>
-//   )
-// }
